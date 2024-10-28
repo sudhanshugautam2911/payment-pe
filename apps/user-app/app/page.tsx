57e796c0b56249f8ce66402061767e8790504957
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2024-10-28 13:02:26 +0530

    Test: workflow test2

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -5,6 +5,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {

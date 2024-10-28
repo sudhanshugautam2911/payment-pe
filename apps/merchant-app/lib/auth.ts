6ca8f5e2e1808f9f19e8e68bd0a494518235a78c
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2024-10-28 10:43:34 +0530

    Build changes: fix minor issues

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -2,46 +2,58 @@ import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
-    callbacks: {
-      async signIn({ user, account }: {
-        user: {
-          email: string;
-          name: string
-        },
-        account: {
-          provider: "google" | "github"
-        }
-      }) {
-        if (!user || !user.email) {
-          return false;
-        }
-
-        await db.merchant.upsert({
-          select: {
-            id: true
-          },
-          where: {
-            email: user.email
-          },
-          create: {
-            email: user.email,
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          },
-          update: {
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          }
-        });
-
-        return true;
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account }: {
+      user: {
+        email: string;
+        name: string
+      },
+      account: {
+        provider: "google" | "github"
       }
+    }) {
+      if (!user || !user.email) {
+        return false;
+      }
+
+      await db.merchant.upsert({
+        select: {
+          id: true
+        },
+        where: {
+          email: user.email
+        },
+        create: {
+          email: user.email,
+          name: user.name,
+          auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+        },
+        update: {
+          name: user.name,
+          auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+        }
+      });
+
+      return true;
     },
-    secret: process.env.NEXTAUTH_SECRET || "secret"
-  }
+    async session({ session, token }: any) {
+      session.user.id = token.id;
+      session.user.auth_type = token.auth_type;
+      return session;
+    },
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.id = user.id;
+        token.auth_type = user.auth_type;
+      }
+      return token;
+    }
+  },
+  secret: process.env.NEXTAUTH_SECRET || "secret"
+}
